057e10ba2ee540dc6c76d79ae3d6b9a713b0be0d
queragion2726 <queragion2726@gmail.com>
2020-01-13 20:42:59 +0900

    Change an expression of subscribe

--- settings.py
+++ settings.py
@@ -149,7 +149,7 @@ CANCELED_NOTICE_MESSAGE = """
 SUBSCRIBE_KEYWORD = '!구독'
 UNSUBSCRIBE_KEYWORD = '!구독해제'
 
-NO_SUCH_USER = '그러한 유저가 존재하지 않습니다.'
+NO_SUCH_USER = '구독 목록에 존재하지 않습니다.'
 ALREADY_EXISTS = '이미 목록에 추가된 유저입니다.'
 APPEND_SUCCESS = '구독에 추가해 드렸습니다.'
 DELETE_SUCCESS = '구독에서 제거해 드렸습니다.'
