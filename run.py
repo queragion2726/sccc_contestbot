dcb77817bb960bcaae049d66ed5c2526f5255092
queragion2726 <queragion2726@gmail.com>
2020-02-13 02:34:45 +0900

    Make enable BaekjoonCollector

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from settings import TOKEN
 import os
 from contestBot import ContestBot
 from collectors.codeforcesCollector import CodeforcesCollector
+from collectors.baekjoonCollector import BaekjoonCollector
 
 bot = ContestBot(TOKEN)
 bot.addCollector(CodeforcesCollector)
+bot.addCollector(BaekjoonCollector)
 bot.start(initNotice=False)
