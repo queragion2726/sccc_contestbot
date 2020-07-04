557085690550f73d76e5a0f9129a7b2264d8cbee
queragion2726 <queragion2726@gmail.com>
2020-07-05 02:56:07 +0900

    ContestManager 기본 골격

---
+++ sccc_contestbot/contest_manager.py
@@ -0,0 +1,17 @@
+def ContestManager():
+    """
+    컨테스트를 관리합니다.
+    """
+
+    def __init__(self, event_loop, thread_local_data):
+        self.event_loop = event_loop
+        self.thread_local_data = thread_local_data
+
+    async def renewal_contest():
+        pass
+
+    async def delete_contest():
+        pass
+
+    async def is_latest():
+        pass
